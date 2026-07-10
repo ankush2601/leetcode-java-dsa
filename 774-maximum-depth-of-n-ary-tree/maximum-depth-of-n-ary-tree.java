2b3f430bbe6966ff0cb097530425661c5e35c867
Ankush Chauhan <ankushapl2601@gmail.com>
2026-07-11 01:41:04 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (77.13%) - LeetSync

--- 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.java
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.java
@@ -19,29 +19,13 @@ class Node {
 
 class Solution {
     public int maxDepth(Node root) {
-        List<List<Integer>> flist = new ArrayList<>();
         if(root == null){
             return 0;
         }
-        Queue<Node> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            int levelNum = q.size();                    
-            List<Integer> list = new ArrayList<>();
-
-            for(int i = 0; i < levelNum; i++){           //iterate on all nodes of the level
-                Node curr = q.poll();
-                list.add(curr.val);                     //add val of current Node to sublist(list);
-
-                int j = 0;
-                while(j < curr.children.size()){       //add children to the queue
-                    q.offer(curr.children.get(j));
-                    j++;
-                }
-               
-            }
-            flist.add(list);
+        int max = 1;
+        for(Node child:root.children){
+            max = Math.max(max,1+maxDepth(child));
         }
-        return flist.size();
+        return max;
     }
 }
