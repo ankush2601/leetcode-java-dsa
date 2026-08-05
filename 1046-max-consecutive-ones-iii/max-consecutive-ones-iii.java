7d9961a88647d7538803aea3d64e1abbd58f7541
Ankush Chauhan <ankushapl2601@gmail.com>
2026-08-06 01:00:24 +0530

    Time: 2564 ms (5.00%) | Memory: 51.4 MB (99.60%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -5,16 +5,20 @@ class Solution {
         for(int i = 0; i < n; i++){
             int zero = 0;
             for(int j = i; j < n; j++){
-                //check is it 1
-                if(nums[j] == 0){
-                    zero++;
-                }
-                if(zero > k){
+                //check is it 0,
+              
+                    if(nums[j] == 0){
+                        zero++;
+                    }
+
+                    if(zero > k){
                     break;
-                }
-                else{
-                    maxCount = Math.max(maxCount,j-i+1);
-                }
+                    }
+                    else{
+                        maxCount = Math.max(maxCount,j-i+1);
+                    }
+                
+                
             }
         }
         return maxCount;
