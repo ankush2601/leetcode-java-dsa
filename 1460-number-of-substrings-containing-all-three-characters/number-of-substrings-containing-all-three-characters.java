b6aaaa38389c2f5ce6a3868b73e85d3c504bf599
Ankush Chauhan <ankushapl2601@gmail.com>
2026-08-12 00:24:05 +0530

    Time: 14 ms (57.31%) | Memory: 46.4 MB (42.17%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -8,8 +8,8 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             arr[s.charAt(i)-'a'] = i;
             if(arr[0] != -1 && arr[1] != -1 && arr[2] != -1 ){
-                c += 1; 
-                c +=  Math.min(arr[0],Math.min(arr[1],arr[2])) ;
+                 
+                c +=  Math.min(arr[0],Math.min(arr[1],arr[2])) + 1;
             }
         }
         return c;
